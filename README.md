b0a7b06bbbc8665ccc4ece6cfada04487e82905b
chrisbodhi <cboette@gmail.com>
2014-03-31 10:34:08 -0500

    Clean up cover retrieval code before merge with master. Update README and .gitignore.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 #####New Things For This Project (in no particular order)
 - node.js server
 - using node's crypto module to create an MD5 hash 
-- promises! [the thing that makes this whole thing work]
-- environmental variables for node.js [not yet, but soon!]
+- Q library &amp; promises! [the thing that makes this whole thing work]
+- environmental variables for node.js
 - more stuff
 
 
--- basic.js
+++ basic.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var crypto = require('crypto');
 var Q = require('q');
-var md5 = crypto.createHash('md5');
 var dotenv = require('dotenv');
 
-// Prepare the environmental variables, aka keys
+// Prepare the md5 for making server-side call to API
+// http://developer.marvel.com/documentation/authorization
+//   -> "Authentication for Server-Side Applications"
+
+var md5 = crypto.createHash('md5');
+
+// Prepare the environmental variables, aka keys, stored in .env
 dotenv.load()
 
 // Placeholder test name - to be stored in a db or pulled from a form later
@@ -12,12 +17,13 @@ var first = 'warren';
 var last = 'ellis';
 
 // ts is a time stamp changed to a string for the URL
-// var ts = new Date().toTimeString();
 var ts = new Date().toTimeString();
+
+// use the dotenv package to retrieve keys
 var private_key = process.env.MARVEL_PRIVATE;
 var public_key = process.env.MARVEL_PUBLIC;
 
-// MD5 hash created per docs stated need for such a combination of information to make a call from the server; that is, node.js
+// Hash created per dev docs to make the server-side call
 var hash = md5.update(ts + private_key + public_key);
 
 // hash.digest('hex') to turn hash into string of letters and numbers for public consumption
@@ -29,8 +35,14 @@ var requisite = "ts=" + ts + "&apikey=" + public_key + "&hash=" + hexed;
 // URL for making search based on creator name
 var creatorURL = "http://gateway.marvel.com/v1/public/creators?firstName=" + first + "&lastName=" + last + "&" + requisite;
 
-// Starts the promises chain!
+//
+// Start of the promises chain!
+// Uses the Q.defer() helper method from the Q library
+//   Creates a promise object, updates it with .resolve,
+//   then passes return value using .promise at the end
+//
 // Uses http.get to hit the API, returns the URI of the creator's comics
+//   ex: http://gateway.marvel.com/v1/public/creators/:creatorID/comics
 var getComicsUri = function(uri){
   "use strict";
   var deferred = Q.defer();
@@ -46,7 +58,7 @@ var getComicsUri = function(uri){
     res.on('end', function(chunk) {
       var responseJSON = JSON.parse(body);
       var comicsCollectionUri = responseJSON.data.results[0].comics.collectionURI;
-      // console.log(comicsCollectionUri, "getComicsUri");
+      console.log(comicsCollectionUri, "getComicsUri");
       deferred.resolve(comicsCollectionUri);
     });
   }).on('error', function(e) {
@@ -57,6 +69,7 @@ var getComicsUri = function(uri){
 
 // Takes the comicsCollection URI, adds the requisite data for making a
 // server-side call, gets a JSON object of the comics
+// Returns only the first 20 entries currently
 var getComicsFunction = function(collectionUri){
   "use strict";
   var deferred = Q.defer();
@@ -82,6 +95,13 @@ var getComicsFunction = function(collectionUri){
   return deferred.promise;
 };
 
+// Creates an array of image tags to be used on the frontend
+// Image dimensions are specified by 'portrait_xlarge': 150 x 225
+//   More dimension info: 
+//     http://developer.marvel.com/documentation/images
+// ex: [ '<img src="http://i.annihil.us/u/prod/marvel/i/mg/9/20/
+//   533475ade25bc/portrait_xlarge.jpg" 
+//   alt="Moon Knight (2014) #2" >', ..]
 var getCovers = function(json) {
   "use strict";
   var imageTags = [];
@@ -90,28 +110,35 @@ var getCovers = function(json) {
   for (var i = 0; i < json.length; i += 1 ) {
     imageTags.push('<img src="' + json[i].thumbnail.path + '/portrait_xlarge.' + json[i].thumbnail.extension + '" alt="' + json[i].title + '" >');
   }
-  // console.log(imageTags, "imageTags");
+  console.log(imageTags, "imageTags");
   deferred.resolve(imageTags);
   return deferred.promise;
 };
 
+// Array to be populated, then passed into the createServer
+// function for display on screen
 var list = [];
 
+// Populates the list array with the image tags from getCovers
 var showMe = function(arr) {
   "use strict";
   list = arr;
-  console.log(list, "list");
+  // console.log(list, "list");
   return list;
 };
 
 // The promise chain that drives this bicycle wheel of nodeness
+// Start with a creator name, end with an array of image tags
+//   of their 20 most recent works
+//
 // TODO: add function(reason) for fails for each dot-then
 getComicsUri(creatorURL)
   .then(getComicsFunction)
   .then(getCovers)
   .then(showMe);
 
-// Running `node basic.js` in the command line stops the execution above this point. Loading the webpage runs everything below this line.
+// Running `node basic.js` in the command line stops executing code above this point. Loading the webpage runs everything below this line.
+// Why? Don't know. Maybe because showMe() has a return?
 
 console.log("list is ", typeof(list));
 console.log("what it is ");
@@ -119,9 +146,6 @@ console.log("what it is ");
 // Make server. Run functions to make API calls. Display images.
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  // console.log('before image is made: ', warrenEllis);
-  // var template = "<img src='" + warrenEllis + "' />"
-  // response.end('Hello <b>World</b>\n' + template);
   response.end('Hello <b>World</b>\n' + list);
 }).listen(8124);
 
